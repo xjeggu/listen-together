3bc3b79a05bcfc8354f2ead1fd0e96e9431e6e62
CoronaBringer <65749353+CoronaBringer@users.noreply.github.com>
2023-07-20 20:33:57 -0400

    feat: add auto host request and improve autoreconnect

--- src/client.tsx
+++ src/client.tsx
@@ -1,119 +1,163 @@
-import { io, Socket } from "socket.io-client";
-import LTPlayer from "./ltPlayer";
+import { io, Socket } from 'socket.io-client';
+import LTPlayer from './ltPlayer';
 import React from 'react';
 import BottomInfo from './ui/bottomInfo';
-import { forcePlayTrack, getCurrentTrackUri, getTrackType, isListenableTrackType } from "./spotifyUtils";
+import {
+  forcePlayTrack,
+  getCurrentTrackUri,
+  getTrackType,
+  isListenableTrackType,
+} from './spotifyUtils';
 
 export default class Client {
-  connecting = false
-  connected = false
-  socket: Socket | null = null
-  server = ""
+  connecting = false;
+  connected = false;
+  socket: Socket | null = null;
+  server = '';
 
-  constructor (public ltPlayer: LTPlayer) {
+  constructor(public ltPlayer: LTPlayer) {
     setInterval(async () => {
       if (this.connected) {
         try {
-          await fetch(this.server)
+          await fetch(this.server);
         } catch {}
       }
-    }, 5*60_000);
+    }, 5 * 60_000);
   }
-  
-  connect(server?: string) {
-    if (!server)
-      server = this.ltPlayer.settingsManager.settings.server
 
-    if (getCurrentTrackUri() != "") {
-      forcePlayTrack("")
-      setTimeout(() => this.connect(server), 100)
+  connect(server?: string) {
+    if (!server) server = this.ltPlayer.settingsManager.settings.server;
+
+    if (getCurrentTrackUri() != '') {
+      forcePlayTrack('');
+      setTimeout(() => this.connect(server), 100);
       return;
     }
 
     this.server = server;
 
     this.connecting = true;
-    this.ltPlayer.ui.renderBottomInfo(<BottomInfo server={server} loading={true} />)
+    this.ltPlayer.ui.renderBottomInfo(
+      <BottomInfo server={server} loading={true} />,
+    );
     // this.ltPlayer.ui.menuItems.joinServer?.setName("Leave the server")
-    
-    this.socket = io(server, {
-      'secure':true,
-    })
 
-    this.socket.on("connect", () => {
-      this.ltPlayer.ui.renderBottomInfo(<BottomInfo server={server!} />)
-      this.connecting = false
-      this.connected = true
-      this.ltPlayer.isHost = false
-      this.socket!.emit("login", this.ltPlayer.settingsManager.settings.name, this.ltPlayer.version, (versionRequirements: string) => {
-        this.socket?.disconnect()
-        setTimeout(() => this.ltPlayer.ui.windowMessage(`Your Spotify Listen Together's version isn't compatible with the server's version. Consider switching to a version that meets these requirements: "${versionRequirements}".`), 1)
-      })
-      this.ltPlayer.onLogin()
-    })
+    this.socket = io(server, {
+      secure: true,
+    });
+
+    this.socket.on('connect', () => {
+      this.ltPlayer.ui.renderBottomInfo(<BottomInfo server={server!} />);
+      this.connecting = false;
+      this.connected = true;
+      this.ltPlayer.isHost = false;
+      this.socket!.emit(
+        'login',
+        this.ltPlayer.settingsManager.settings.name,
+        this.ltPlayer.version,
+        (versionRequirements: string) => {
+          this.socket?.disconnect();
+          setTimeout(
+            () =>
+              this.ltPlayer.ui.windowMessage(
+                `Your Spotify Listen Together's version isn't compatible with the server's version. Consider switching to a version that meets these requirements: "${versionRequirements}".`,
+              ),
+            1,
+          );
+        },
+      );
+      this.ltPlayer.onLogin();
+
+      // Try to request host if password is set
+      const password = this.ltPlayer.settingsManager.settings.password;
+      console.log(`Password: ${password}`);
+      if (password != '') {
+        this.socket!.emit('requestHost', password);
+      }
+    });
 
     this.socket.onAny((ev: string, ...args: any) => {
-      console.log(`Receiving ${ev}: ${args}`)
-    })
+      console.log(`Receiving ${ev}: ${args}`);
+    });
 
-    this.socket.on("changeSong", (trackUri: string) => {
+    this.socket.on('changeSong', (trackUri: string) => {
       if (isListenableTrackType(getTrackType(trackUri)))
-        this.ltPlayer.onChangeSong(trackUri)
-    })
+        this.ltPlayer.onChangeSong(trackUri);
+    });
 
-    this.socket.on("updateSong", (pause: boolean, milliseconds: number) => {
+    this.socket.on('updateSong', (pause: boolean, milliseconds: number) => {
       if (isListenableTrackType())
-        this.ltPlayer.onUpdateSong(pause, milliseconds)
-    })
+        this.ltPlayer.onUpdateSong(pause, milliseconds);
+    });
 
-    this.socket.on("bottomMessage", (message: string) => {
-      this.ltPlayer.ui.bottomMessage(message)
-    })
+    this.socket.on('bottomMessage', (message: string) => {
+      this.ltPlayer.ui.bottomMessage(message);
+    });
 
-    this.socket.on("windowMessage", (message: string) => {
-      this.ltPlayer.ui.windowMessage(message)
-    })
+    this.socket.on('windowMessage', (message: string) => {
+      this.ltPlayer.ui.windowMessage(message);
+    });
 
-    this.socket.on("listeners", (clients: any) => {
-      this.ltPlayer.ui.renderBottomInfo(<BottomInfo server={server!} listeners={clients} />)
-    })
+    this.socket.on('listeners', (clients: any) => {
+      this.ltPlayer.ui.renderBottomInfo(
+        <BottomInfo server={server!} listeners={clients} />,
+      );
+    });
 
-    this.socket.on("isHost", (isHost: boolean) => {
+    this.socket.on('isHost', (isHost: boolean) => {
       if (isHost != this.ltPlayer.isHost) {
-        this.ltPlayer.isHost = isHost
+        this.ltPlayer.isHost = isHost;
         if (isHost) {
           // this.ltPlayer.ui.menuItems.requestHost?.setName("Cancel hosting");
-          this.ltPlayer.ui.bottomMessage("You are now a host.")
+          this.ltPlayer.ui.bottomMessage('You are now a host.');
         } else {
           // this.ltPlayer.ui.menuItems.requestHost?.setName("Request host");
-          this.ltPlayer.ui.bottomMessage("You are no longer a host.")
+          this.ltPlayer.ui.bottomMessage('You are no longer a host.');
         }
       }
-    })
+    });
 
-    this.socket.on("songRequested", (trackUri: string, trackName: string, fromListener: string) => {
-      this.ltPlayer.ui.songRequestPopup(trackName, fromListener, () => {
-        forcePlayTrack(trackUri)
-      })
-    })
-  
-    this.socket.on("disconnect", () => this.disconnect())
+    this.socket.on(
+      'songRequested',
+      (trackUri: string, trackName: string, fromListener: string) => {
+        this.ltPlayer.ui.songRequestPopup(trackName, fromListener, () => {
+          forcePlayTrack(trackUri);
+        });
+      },
+    );
 
-    this.socket.on("error", () => {
-      this.disconnect()
-      this.ltPlayer.ui.windowMessage(`Couldn't connect to "${server}".`)
-    })
+    this.socket.on('connect_error', () => {
+      setTimeout(() => {
+        console.log('Retrying to connect...');
+        this.connect(this.server);
+      }, 1000);
+    });
+
+    this.socket.on('disconnect', (reason) => {
+      console.log(`Disconnected: ${reason}`);
+      if (reason === 'io server disconnect') {
+        this.connect(this.server);
+      } else if (reason === 'io client disconnect') {
+        this.disconnect();
+      }
+    });
+
+    this.socket.on('error', () => {
+      console.log('Error connecting to the server.');
+      this.disconnect();
+      this.ltPlayer.ui.windowMessage(`Couldn't connect to "${server}".`);
+    });
   }
-  
+
   disconnect() {
-    this.socket?.disconnect()
-    this.socket = null
-    this.connected = false
-    this.ltPlayer.isHost = false
-    this.connecting = false
+    this.socket?.disconnect();
+    this.socket = null;
+    this.connected = false;
+    this.ltPlayer.isHost = false;
+    this.connecting = false;
     // this.ltPlayer.ui.menuItems.joinServer?.setName("Join a server")
     // this.ltPlayer.ui.menuItems.requestHost?.setName("Request host");
-    this.ltPlayer.ui.renderBottomInfo(<BottomInfo server={""}/>)
-    this.ltPlayer.ui.disconnectedPopup()
+    this.ltPlayer.ui.renderBottomInfo(<BottomInfo server={''} />);
+    this.ltPlayer.ui.disconnectedPopup();
   }
 }
--- src/spotifyUtils.ts
+++ src/spotifyUtils.ts
@@ -1,5 +1,5 @@
-import LTPlayer from "./ltPlayer"
-import { OGFunctions } from "./patcher"
+import LTPlayer from './ltPlayer';
+import { ogPlayerAPI } from './patcher';
 
 export enum TrackType {
   Ad,
@@ -10,45 +10,42 @@ export enum TrackType {
 }
 
 export async function getTrackData(trackUri: string) {
-  if (isTrack(trackUri))
-    trackUri = trackUri.split(":")[2]
+  if (isTrack(trackUri)) trackUri = trackUri.split(':')[2];
 
-  let token = await Spicetify.CosmosAsync.get("sp://auth/v2/token");
+  let token = await Spicetify.CosmosAsync.get('sp://auth/v2/token');
   return await fetch(`https://api.spotify.com/v1/tracks/${trackUri}`, {
-    "headers": { "authorization": "Bearer " + token.accessToken }
-  }).then(a => a.json())
+    headers: { authorization: 'Bearer ' + token.accessToken },
+  }).then((a) => a.json());
 }
 
 export function getCurrentTrackUri(): string {
-  return (Spicetify.Platform.PlayerAPI._state.item?.uri) || ""
+  return Spicetify.Platform.PlayerAPI._state.item?.uri || '';
 }
 
 export function isListenableTrackType(trackType?: TrackType) {
-  if (trackType === undefined)
-    trackType = getTrackType()
+  if (trackType === undefined) trackType = getTrackType();
 
-  return trackType === TrackType.Episode || trackType === TrackType.Track
+  return trackType === TrackType.Episode || trackType === TrackType.Track;
 }
 
 export function getTrackType(trackUri?: string): TrackType {
-  if (trackUri === undefined)
-    trackUri = getCurrentTrackUri()
+  if (trackUri === undefined) trackUri = getCurrentTrackUri();
 
-  switch ((trackUri.split(":")[1] || "").toLowerCase()) {
-    case "": {
-      return TrackType.NoTrack
+  switch ((trackUri.split(':')[1] || '').toLowerCase()) {
+    case '': {
+      return TrackType.NoTrack;
     }
-    case "ad": {
-      return TrackType.Ad
+    case 'ad': {
+      return TrackType.Ad;
     }
-    case "track": {
-      return TrackType.Track
+    case 'track': {
+      return TrackType.Track;
     }
-    case "episode": {
-      return TrackType.Episode
+    case 'episode': {
+      return TrackType.Episode;
     }
     default: {
-      return TrackType.Unknown
+      return TrackType.Unknown;
     }
   }
 }
@@ -58,52 +55,64 @@ export function isTrack(trackUri: string): boolean {
 }
 
 export function pauseTrack() {
-  if (Spicetify.Player.isPlaying()) OGFunctions.pause()
+  if (Spicetify.Player.isPlaying()) ogPlayerAPI.pause();
 }
 
 export function resumeTrack() {
-  if (!Spicetify.Player.isPlaying()) OGFunctions.resume()
+  if (!Spicetify.Player.isPlaying()) ogPlayerAPI.resume();
 }
 
 export function isTrackPaused() {
-  return !Spicetify.Player.isPlaying()
+  return !Spicetify.Player.isPlaying();
 }
 
 export function getTrackProgress() {
-  return Spicetify.Player.getProgress()
+  return Spicetify.Player.getProgress();
 }
 
 export function forcePlayTrack(trackUri: string): void {
-  forcePlay({ uri: trackUri }, {}, {})
+  forcePlay({ uri: trackUri }, {}, {});
 }
 
 export function forcePlay(uri: any, origins: any, options: any): void {
-  Spicetify.Platform.PlayerAPI.play(uri, origins, {...options, ltForced: true})
+  Spicetify.Platform.PlayerAPI.play(uri, origins, {
+    ...options,
+    ltForced: true,
+  });
 }
-  
-export class SpotifyUtils {
-  constructor (public ltPlayer: LTPlayer) {}
 
-  loadedInterval: NodeJS.Timer | null = null
-  timeoutLoadedCallback: NodeJS.Timeout | null = null
-  
+export class SpotifyUtils {
+  constructor(public ltPlayer: LTPlayer) {}
+
+  loadedInterval: NodeJS.Timer | null = null;
+  timeoutLoadedCallback: NodeJS.Timeout | null = null;
+
   onTrackLoaded(trackUri: string, callback: () => void) {
     if (this.loadedInterval) clearInterval(this.loadedInterval);
-    if (this.timeoutLoadedCallback) clearTimeout(this.timeoutLoadedCallback)
+    if (this.timeoutLoadedCallback) clearTimeout(this.timeoutLoadedCallback);
 
     this.loadedInterval = setInterval(() => {
-      console.log(`check loaded: ${getCurrentTrackUri()}===${trackUri}   ${Spicetify.Platform.PlayerAPI._state?.item?.name}  ${!Spicetify.Platform.PlayerAPI._state.isBuffering}`)
-      if (getCurrentTrackUri() === trackUri && Spicetify.Platform.PlayerAPI._state?.item?.name && !Spicetify.Platform.PlayerAPI._state.isBuffering) {
-        if (this.loadedInterval) clearInterval(this.loadedInterval)
-        if (this.timeoutLoadedCallback) clearTimeout(this.timeoutLoadedCallback)
-        callback()
+      console.log(
+        `check loaded: ${getCurrentTrackUri()}===${trackUri}   ${
+          Spicetify.Platform.PlayerAPI._state?.item?.name
+        }  ${!Spicetify.Platform.PlayerAPI._state.isBuffering}`,
+      );
+      if (
+        getCurrentTrackUri() === trackUri &&
+        Spicetify.Platform.PlayerAPI._state?.item?.name &&
+        !Spicetify.Platform.PlayerAPI._state.isBuffering
+      ) {
+        if (this.loadedInterval) clearInterval(this.loadedInterval);
+        if (this.timeoutLoadedCallback)
+          clearTimeout(this.timeoutLoadedCallback);
+        callback();
       }
-    }, 100)
+    }, 100);
 
     this.timeoutLoadedCallback = setTimeout(() => {
-      if (this.timeoutLoadedCallback) clearTimeout(this.timeoutLoadedCallback)
-      if (this.loadedInterval) clearInterval(this.loadedInterval)
-      callback()
+      if (this.timeoutLoadedCallback) clearTimeout(this.timeoutLoadedCallback);
+      if (this.loadedInterval) clearInterval(this.loadedInterval);
+      callback();
     }, 5000);
 
     return this.loadedInterval;
